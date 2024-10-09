12b47ae24c5c60b95789a0cf562bc11d462337f7
Aidar Putilov <putiloff@gmail.com>
2024-10-10 03:39:58 +0500

    Edit def createusers()

--- users/management/commands/createusers.py
+++ users/management/commands/createusers.py
@@ -20,19 +20,3 @@ class Command(BaseCommand):
                     user.is_staff = True
                     user.is_superuser = True
                 user.save()
-
-
-#        user = User.objects.create(email="admin@sky.pro")
-#        user.set_password("123qwe")
-#        user.is_active = True
-#        user.is_staff = True
-#        user.is_superuser = True
-#        user.save()
-#        user = User.objects.create(email="user1@sky.pro")
-#        user.set_password("123qwe")
-#        user.is_active = True
-#        user.save()
-#        user = User.objects.create(email="user2@sky.pro")
-#        user.set_password("123qwe")
-#        user.is_active = True
-#        user.save()
