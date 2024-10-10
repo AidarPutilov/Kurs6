77b3b08732e6bb7cc110b19bda44b85346cf8428
Aidar Putilov <putiloff@gmail.com>
2024-10-11 01:52:11 +0500

    Create model Client

--- main/models.py
+++ main/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from config.settings import AUTH_USER_MODEL
+
+class Client(models.Model):
+    email = models.EmailField(
+        verbose_name='электронная почта',
+        unique=True
+    )
+    fio = models.CharField(
+        max_length=100,
+        verbose_name='ФИО'
+    )
+    comment = models.TextField(
+        verbose_name='комментарий',
+        null=True, blank=True
+    )
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='пользователь',
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.fio
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+
+
+# class MailingList(models.Model):
+#     pass
+
+
+# class Massage(models.Model):
+#     pass
+
+
+# class Attempt(models.Model):
+#     pass
