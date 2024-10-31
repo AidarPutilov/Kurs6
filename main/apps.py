ec05358bd13c3d165da2b12514f1d8e1c504f876
Aidar Putilov <putiloff@gmail.com>
2024-11-01 00:24:51 +0500

    Edit README.md

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,12 @@
+from time import sleep
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self) -> None:
+        from main.task import send_mailings
+        sleep(2)
+        send_mailings()
