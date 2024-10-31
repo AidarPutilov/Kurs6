171c45addb699ed0b66464b3849cedd4538cfe37
Aidar Putilov <putiloff@gmail.com>
2024-11-01 02:46:47 +0500

    Release blog

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from blog.views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
+from blog.apps import BlogConfig
+
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('list_blog', BlogListView.as_view(), name='list_blog'),
+    path('detail_blog/<int:pk>/', BlogDetailView.as_view(), name='detail_blog'),
+    path('create_blog/', BlogCreateView.as_view(), name='create_blog'),
+    path('update_blog/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
+    path('delete_blog/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog'),
+]
