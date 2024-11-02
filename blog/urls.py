b29b3b7607ca3161b02d543e5b68fae63cb4b0c8
Aidar Putilov <putiloff@gmail.com>
2024-11-02 14:03:00 +0500

    Cache enebled

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from blog.views import (
     BlogListView,
     BlogDetailView,
@@ -13,7 +14,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('list_blog', BlogListView.as_view(), name='list_blog'),
-    path('detail_blog/<int:pk>/', BlogDetailView.as_view(), name='detail_blog'),
+    path('detail_blog/<int:pk>/', cache_page(60)(BlogDetailView.as_view()), name='detail_blog'),
     path('create_blog/', BlogCreateView.as_view(), name='create_blog'),
     path('update_blog/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
     path('delete_blog/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog'),
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from main.apps import MainConfig
 from main.views import (
@@ -28,7 +29,7 @@ from main.views import (
 app_name = MainConfig.name
 
 urlpatterns = [
-    path("", MailingHome.as_view(), name="main"),
+    path("", cache_page(60)(MailingHome.as_view()), name="main"),
     # Маршруты Client
     path("listclient/", ClientListView.as_view(), name="list_client"),
     path("clientactive/<int:pk>/", toggle_client_active, name="active_client"),
